1afd956deffbd50e1f86aeea4dd05f8c48e8dd88
Sergey Zavitaev <nikgala@list.ru>
2023-07-11 18:47:33 +0300

    Task 2 is complected

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления 
+//функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Random rand = new Random();
+
+int AckermannFunction(int M, int N)
+{
+    if (M == 0) { return (N+1); }
+    if (M>0 & N==0) { return AckermannFunction(M - 1, 1); }
+    if (M > 0 & N > 0)
+    { //Console.WriteLine(", m- " + M + ":,n-" + N);
+        return AckermannFunction(M - 1,
+        AckermannFunction(M, N - 1) ); 
+    }
+    else { return -1; }
+}
+
+int NumM = rand.Next(1, 4);
+Console.WriteLine($"Число M = {NumM}");
+int NumN = rand.Next(1, 6);
+Console.WriteLine($"Число N = {NumN}");
+
+Console.WriteLine(AckermannFunction(NumM, NumN));
